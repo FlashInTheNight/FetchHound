a4ce9d8fb82510a868d18c181146c1cf24a026f2
John_the_FlashInTheNight <thecreepystyle@gmail.com>
2025-04-24 14:45:14 +0500

    Unit testing of the video scanning logic has been completed.

--- tests/scanVideosSites.test.ts
+++ tests/scanVideosSites.test.ts
@@ -34,4 +34,29 @@ describe("Scan videos in sites HTML", () => {
     );
     expect(duplicates).toHaveLength(2);
   });
+
+  it("find video item in reddit", () => {
+    const video = scanVideos();
+    expect(video).toEqual(
+      expect.arrayContaining([
+        {
+          url: "https://packaged-media.redd.it/6r1tvkw8blwe1/pb/m2-res_594p.mp4?m=DASHPlaylist.mpd&v=1&e=1745488800&s=22f1f38e1fab0a0aed9fd9c3ff01a53ecb5c116f",
+          thumb:
+            "https://external-preview.redd.it/body-armor-company-demonstrates-their-stab-protection-on-v0-NTVveGF2MDlibHdlMWBD0rlOZOc1bhiSmvEHWUOlQkWTvVq1wWwxf5uPRr2m.png?width=640&crop=smart&format=pjpg&auto=webp&s=507310a433a22697cbcba1bc518eee24fb0be23a",
+        },
+      ])
+    );
+  });
+
+  it("find video item in 4chan", () => {
+    const video = scanVideos();
+    expect(video).toEqual(
+      expect.arrayContaining([
+        {
+          url: "https://i.4cdn.org/wsg/1612345678901.webm",
+          thumb: null,
+        },
+      ])
+    );
+  });
 });
